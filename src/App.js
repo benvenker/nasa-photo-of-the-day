8abdf674c25f84906f470e2a14715e57e21afab8
Ben Venker <bvemails@gmail.com>
2020-03-10 20:17:56 -0700

    create skeleton components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Title from "./components/Title";
+import Image from "./components/Image";
+import Description from "./components/Description";
+import Datepicker from "./components/Datetpicker";
 
 function App() {
+  const [images, setImage] = useState([]);
+
+  useEffect(() => {});
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Title />
     </div>
   );
 }
---
+++ src/components/Datetpicker.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Datepicker = () => {};
+
+export default Datepicker;
