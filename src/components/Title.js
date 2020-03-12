e7e55f1096724d3894546f576d904923e1e51574
Ben Venker <bvemails@gmail.com>
2020-03-11 17:42:21 -0700

    remove excess code

--- src/components/Title.js
+++ src/components/Title.js
@@ -19,13 +19,4 @@ const Title = props => {
   );
 };
 
-// const Title = props => {
-//   return (
-//     <>
-//       <h2>NASA Photo of the Day: {props.date}</h2>
-//       <h1>{props.title}</h1>
-//     </>
-//   );
-// };
-
 export default Title;
