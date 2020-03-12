b23f3af31b4d9b00501a22d23bf6fcea630d085e
Ben Venker <bvemails@gmail.com>
2020-03-11 17:06:59 -0700

    convert Title component to styled component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./Title.css";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-weight: bold;
+`;
+
+const H2 = styled.h2`
+  font-weight: bold;
+`;
 
 const Title = props => {
   return (
     <>
-      <h2>NASA Photo of the Day: {props.date}</h2>
-      <h1>{props.title}</h1>
+      <H2>Nasa Photo of the Day: {props.date}</H2>
+      <H1>{props.title}</H1>
     </>
   );
 };
+
+// const Title = props => {
+//   return (
+//     <>
+//       <h2>NASA Photo of the Day: {props.date}</h2>
+//       <h1>{props.title}</h1>
+//     </>
+//   );
+// };
 
 export default Title;
